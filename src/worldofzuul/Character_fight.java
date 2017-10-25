bb89b0e84f32cd4c3d21c2830db6f0d5b105ff93
jeanjohnsen <Geniusnh4@gmail.com>
2017-10-25 12:12:36 +0200

    added constructor

--- src/worldofzuul/Character_fight.java
+++ src/worldofzuul/Character_fight.java
@@ -5,6 +5,7 @@ public class Character_fight
     //Har brugt samme variables som i monster på nær name og boolean.
     private String name;
     private int hp;
+    private int dmg;
     private boolean isDead = false;
 
     //Character attributer
@@ -12,20 +13,20 @@ public class Character_fight
     {
         this._name = name;
         this._hp = hp;
+        this._dmg = dmg;
     }
 
     public void attack(Character_fight _dmg) 
     {
-        int dmg = 0;
-        //udregn damage
+        dmg = 0;
         dmg = (Math.random()*11);
         _dmg.takeDamage(dmg);
     }
 
-    public void takeDamage(int dmg) 
+    public void setHp(int dmg) 
     {
         //damage man tager
-        //Hvis dmg er større end hp og det er <= 0: død
+        //Hvis dmg er større end hp eller det er = 0: død
         if (hp - dmg <= 0) 
         {
             isDead = true;
@@ -35,9 +36,22 @@ public class Character_fight
             hp -= dmg;
         }
     }
+
+    public getHp()
+    {
+        return this.hp;
+
+    }
+
+    public getDmg()
+    {
+        return this._dmg;
+    }
+
     //Ellers return død
     public boolean isDead() 
     {
         return isDead;
     }
+
 }
