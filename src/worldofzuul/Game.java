7d7f5f4dc764258da5a9786a5118f3311175d9e9
aqibsajjad <32340194+aqibsajjad@users.noreply.github.com>
2017-10-25 10:31:39 +0200

    Update Game.java

--- src/worldofzuul/Game.java
+++ src/worldofzuul/Game.java
@@ -18,41 +18,31 @@ public class Game
 
     private void createRooms()
     {
-         Room outside, grydehutten, cafeteria, U55, basement, U1, fitness, iFitness, bib, hallway, TEK, hallway2;
+         Room outside, grydehutten, cafeteria, U55, basement,bib, hallway, TEK;
 
         outside = new Room("outside the main entrance of the university");
         grydehutten = new Room("");
         cafeteria = new Room("in the cafeteria");
         U55 = new Room("in lecture room U55");
         basement = new Room("in the basement of the university");
-        U1 = new Room ("in lecture room U1");
         hallway = new Room ("in the long hallway in front of room U55");
         bib = new Room (" in the university's library");
-        fitness = new Room ("in front of the intrance to the gym");
-        iFitness = new Room ("in the gym");
         TEK = new Room ("in front of the bronze stairs at TEK");
-        hallway2 = new Room ("in the small hallway in front of lecture room U1");
+        
        
 
         outside.setExit("TEK", TEK);
         hallway.setExit("U55", U55);
         hallway.setExit("cafeteria", cafeteria);
         outside.setExit("basement", basement);
-        bib.setExit("hallway 2", hallway2);
-        outside.setExit("aroundUni", fitness);
         hallway.setExit("bib", bib);
         outside.setExit("hallway", hallway);
         outside.setExit("", grydehutten);
-        U1.setExit("hallway 2", hallway2);
-        iFitness.setExit("",fitness);
         grydehutten.setExit("west", outside);
         cafeteria.setExit("", outside);
         U55.setExit("hallway", hallway);        
         hallway.setExit("basement", basement);
         basement.setExit("hallway", hallway);
-        hallway2.setExit("bib", bib);
-        hallway2.setExit("U1", U1);
-        fitness.setExit("i fitness", iFitness);
         TEK.setExit("outside", outside);
         TEK.setExit("hallway", hallway);
         cafeteria.setExit("hallway", hallway);
