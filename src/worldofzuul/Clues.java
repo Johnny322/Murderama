a189277ee9941ef5accb28f7686a89b0367578b0
Lasat17 <33085898+Lasat17@users.noreply.github.com>
2017-12-13 16:08:24 +0100

    Update Clues.java

--- src/worldofzuul/Clues.java
+++ src/worldofzuul/Clues.java
@@ -3,31 +3,47 @@ package worldofzuul2;
 import java.util.HashMap;
 
 public class Clues {
-	
+
+    /**
+     * Creates a HashMap with Strings and clues, which is called listOfClues.
+     */
     private final HashMap<String, Clue> listOfClues;
-	
-	public Clues() {
+
+    /**
+     * Constructor which is used instantiate clues.
+     */
+    public Clues() {
         listOfClues = new HashMap<String, Clue>();
-        for(Clue clue : Clue.values()) {
-            if(clue != Clue.UNKNOWN) {
+        for (Clue clue : Clue.values()) {
+            if (clue != Clue.UNKNOWN) {
                 listOfClues.put(clue.toString(), clue);
             }
         }
-	}
-	
-	public Clue getClue(String clue)
-    {
+    }
+
+    /**
+     * Accessor which accesses the clue from listOfClues
+     *
+     * @param clue - it is a clue from the list.
+     * @return it is the clue from the list.
+     *
+     */
+    public Clue getClue(String clue) {
         Clue temp = listOfClues.get(clue);
-        if(temp != null) {
+        if (temp != null) {
             return temp;
-        }
-        else {
+        } else {
             return Clue.UNKNOWN;
         }
     }
-	
-	public boolean isClue(String aString)
-    {
+
+    /**
+     * It clue is in the list by checking for key(clue).
+     *
+     * @param aString it is the key.
+     * @return it returns the clue.
+     */
+    public boolean isClue(String aString) {
         return listOfClues.containsKey(aString);
     }
 }
