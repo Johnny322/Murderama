f0211a90a90ffd15c9968259283d73a9096a93cb
Lasat17 <33085898+Lasat17@users.noreply.github.com>
2017-12-13 16:12:42 +0100

    Update Friendly.java

--- src/worldofzuul/Friendly.java
+++ src/worldofzuul/Friendly.java
@@ -5,7 +5,6 @@
  */
 package worldofzuul2;
 
-
 /**
  *
  * @author Simon
@@ -20,9 +19,23 @@ public class Friendly extends Character implements NPC {
     private int threshold;
     private String description;
 
-    
-    //Constructor, tager name, damage og hp til Character-constructoren
-    public Friendly (String name, int damage, int hp, String information, boolean isMurderer, boolean friendly, String additionalInformation, int threshold, String description) {
+    /**
+     *
+     * @param name - the name of the Character.
+     * @param damage - the damage the character deals.
+     * @param hp - the amount of health points the character has.
+     * @param information - the information the character knows.
+     * @param isMurderer - if the character is the murderer.
+     * @param friendly - the character is friendly.
+     * @param additionalInformation - if the character has addintional
+     * Information.
+     * @param threshold - how likely a character is to trust the player. The
+     * player needs a likeability-value higher than this to be able to talk to
+     * this character.
+     * @param description - a description of character.
+     */
+
+    public Friendly(String name, int damage, int hp, String information, boolean isMurderer, boolean friendly, String additionalInformation, int threshold, String description) {
         super(name, damage, hp);
         this.information = information;
         this.isMurderer = isMurderer;
@@ -33,61 +46,97 @@ public class Friendly extends Character implements NPC {
         this.description = description;
     }
 
+    /**
+     * Constructor gets the characters Threshold.
+     *
+     * @return the Threshold of the character.
+     */
     @Override
     public int getThreshold() {
         return this.threshold;
     }
-    
+
+    /**
+     * Makes the the character hostile.
+     *
+     * @param isHostile - checks if the character is hostile.
+     */
     @Override
     public void setHostile(boolean isHostile) {
         this.isHostile = isHostile;
     }
 
+    /**
+     * Constructor which checks if the character is hostile.
+     *
+     * @return if true it makes the character hostile.
+     */
     @Override
     public boolean isHostile() {
         return this.isHostile;
     }
-    
 
+    /**
+     * Constructor gets the characters description.
+     *
+     * @return the description of the character.
+     */
     public String getDescription() {
         return description;
     }
 
     /**
+     * Constructor which checks if the character is Murderer.
      *
-     * @return
+     * @return if true it makes the character Murderer.
      */
     @Override
     public boolean isMurderer() {
         return isMurderer;
     }
-    
+
     /**
+     * Constructor gets the characters Information.
      *
-     * @return
+     * @return the Information of the character.
      */
     //Overrider metoden getInformation der er blevet defineret for NPC-interfacet
     @Override
     public String getInformation() {
-        if(this.isHostile) {
+        if (this.isHostile) {
             System.out.println(super.getName() + " is hostile and cannot be talked to");
             return null;
         }
         return this.information;
     }
 
+    /**
+     * Constructor which checks if the character is Evil.
+     *
+     * @return if true it makes the character Evil.
+     */
     @Override
     public boolean isEvil() {
         return false;
     }
-    
+
+    /**
+     * Constructor which checks if the character is friendly.
+     *
+     * @return if true it makes the character friendly.
+     */
     @Override
-    public boolean friendly(){
+    public boolean friendly() {
         return this.friendly;
     }
-    
-    public String getAdditionalInformation() {    
+
+    /**
+     * Constructor gets the characters Additional Information.
+     *
+     * @return the Additional Information of the character.
+     */
+    public String getAdditionalInformation() {
         return this.additionalInformation;
     }
-    
+
 }
