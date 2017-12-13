97fb15eb6be6054e11a6f7eff5aecd7c410b7841
Lasat17 <33085898+Lasat17@users.noreply.github.com>
2017-12-13 16:09:24 +0100

    Update CommandWord.java

--- src/worldofzuul/CommandWord.java
+++ src/worldofzuul/CommandWord.java
@@ -4,18 +4,17 @@ package worldofzuul2;
  * @author Michael Kolling and David J. Barnes
  * @version 2006.03.30
  */
-public enum CommandWord
-{
-    GO("go"), 
-    QUIT("quit"), 
-    HELP("help"), 
-    UNKNOWN("?"), 
-    FIGHT("fight"), 
-    LOOT("loot"), 
-    USE("use"), 
-    MOVE("move"), 
-    ACCUSE("accuse"), 
-    FLEE("flee"), 
+public enum CommandWord {
+    GO("go"),
+    QUIT("quit"),
+    HELP("help"),
+    UNKNOWN("?"),
+    FIGHT("fight"),
+    LOOT("loot"),
+    USE("use"),
+    MOVE("move"),
+    ACCUSE("accuse"),
+    FLEE("flee"),
     TALK("talk"),
     SHOWNOTES("show"),
     W("w"),
@@ -25,25 +24,25 @@ public enum CommandWord
     SEARCH("search"),
     LOAD("load"),
     SAVE("save");
-    
+
     private String commandString;
-    
+
     /**
-     * Constructor for the CommandWord-class
-     * @param commandString 
+     * Constructor for the CommandWord-class.
+     *
+     * @param commandString.
      */
-    CommandWord(String commandString)
-    {
+    CommandWord(String commandString) {
         this.commandString = commandString;
     }
-    
+
     /**
-     * Converts the CommandWord to a String
-     * @return String
+     * Converts the CommandWord to a String.
+     *
+     * @return the CommandWord as a String.
      */
     @Override
-    public String toString()
-    {
+    public String toString() {
         return commandString;
     }
 }
